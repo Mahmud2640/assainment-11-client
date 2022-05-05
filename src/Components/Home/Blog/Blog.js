4f4d67bfdd67cad95f22d7ca90083c7e4bf26f75
Mahmud2640 <mdmahmud484@gmail.com>
2022-05-05 13:17:31 +0600

    banner img added

--- src/Components/Home/Blog/Blog.js
+++ src/Components/Home/Blog/Blog.js
@@ -4,6 +4,8 @@ const Blog = () => {
    return (
       <div>
          <h1>Blogs</h1>
+         <h2>My name is Ashiqual Islam Rajon</h2>
+         <p>My collage name is Raipur Govt college</p>
       </div>
    );
 };
